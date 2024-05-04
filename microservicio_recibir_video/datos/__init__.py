285b8247b02c25ec5a6409e9ed269b4652dec4e8
Eduar Romero <eg.romeror1@uniandes.edu.co>
2024-05-03 21:35:58 -0500

    ajustes para caso de microservicio recibir viceos

--- microservicio_recibir_video/datos/__init__.py
+++ microservicio_recibir_video/datos/__init__.py
@@ -2,15 +2,31 @@ from flask import Flask
 from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv
+import configparser
+
+config = configparser.ConfigParser()
+config.sections()
+config.read('config.ini')
+
+db_user = config['credentials']['db_user']
+db_password = config['credentials']['db_password']
+db_host = config['credentials']['db_host']
+db_port = config['credentials']['db_port']
+db_database = config['credentials']['db_database']
 
 load_dotenv()
-db_user = os.getenv('POSTGRES_USER')
-iam_user = os.getenv('IAM_USER')
-db_password = os.getenv('POSTGRES_PASSWORD')
-db_host = os.getenv('POSTGRES_HOST')
-db_port = os.getenv('POSTGRES_PORT')
-db_database = os.getenv('POSTGRES_DB')
-instance = os.getenv('INSTANCE_CONNECTION_NAME')
+# db_user = os.getenv('POSTGRES_USER')
+# iam_user = os.getenv('IAM_USER')
+# db_password = os.getenv('POSTGRES_PASSWORD')
+# db_host = os.getenv('POSTGRES_HOST')
+# db_port = os.getenv('POSTGRES_PORT')
+# db_database = os.getenv('POSTGRES_DB')
+# instance = os.getenv('INSTANCE_CONNECTION_NAME')
+db_user = config['credentials']['db_user']
+db_password = config['credentials']['db_password']
+db_host = config['credentials']['db_host']
+db_port = config['credentials']['db_port']
+db_database = config['credentials']['db_database']
 
 # Crear la aplicación
 def create_app(config_name):
